0c2125c2583c1d43d3b1c822d873a4a03d37a32a
tomjimseng <tomjimseng@gmail.com>
2020-02-10 12:13:44 -0800

    6ees6ees6ees update

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    return this.honeyPot += 1;
+  }
+
+  giveHoney() {
+    return this.honeyPot -= 1;
+  }
+
 };
